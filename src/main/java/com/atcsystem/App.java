c37182c171b946bc13bda5f2c07b2a672ea2c893
Amina Jiwa <amina.jiwa@yahoo.co.uk>
2025-04-06 23:29:50 +0100

    Create models

--- src/main/java/com/atcsystem/App.java
+++ src/main/java/com/atcsystem/App.java
@@ -12,16 +12,16 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-        // 1. Create a simple "Hello World" label
+        //Create a simple "Hello World" label
         Label helloLabel = new Label("Hello, Air Traffic Control System!");
         
-        // 2. Create a layout and add the label
+        //Create a layout and add the label
         StackPane root = new StackPane(helloLabel);
         
-        // 3. Create a scene with the layout (400x300 pixels)
+        //Create a scene with the layout (400x300 pixels)
         Scene scene = new Scene(root, 400, 300);
         
-        // 4. Set up the stage (window)
+        //Set up the stage (window)
         stage.setTitle("ATC System");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/atcsystem/models/aircraft/Aircraft.java
@@ -0,0 +1,18 @@
+package com.atcsystem.models.aircraft;
+
+public class Aircraft {
+    //Registration number
+    private String registration;
+    private String airline;
+    private String iata;
+    private String icao;
+    private boolean isOnGround;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private double direction;
+    private double speedHorizontal;
+    private double speedVertical;
+    private FlightPhase flightPhase;
+
+}
